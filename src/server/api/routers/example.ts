3b79dd9862ee56f84b51d753eeb3cd30efc3ea95
Vincent Udén <vincent.uden@colmeo.se>
2023-01-04 18:47:41 +0100

    Front end for adding and removing weight entries

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -18,4 +18,35 @@ export const exampleRouter = createTRPCRouter({
   getSecretMessage: protectedProcedure.query(() => {
     return "you can now see this secret message!";
   }),
+
+  getAllWeightEntries: protectedProcedure.query(async ({ ctx }) => {
+    return await ctx.prisma.weightEntry.findMany({
+      where: { userId: ctx.session.user.id },
+    });
+  }),
+
+  addWeightEntry: protectedProcedure
+    .input(
+      z.object({
+        kg: z.number().positive(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      await ctx.prisma.weightEntry.create({
+        data: {
+          userId: ctx.session.user.id,
+          kg: input.kg,
+        },
+      });
+    }),
+
+  deleteWeightEntry: protectedProcedure
+    .input(
+      z.object({
+        id: z.string().cuid(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      await ctx.prisma.weightEntry.delete({ where: { id: input.id } });
+    }),
 });
